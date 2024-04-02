141fd1f9ac73db9b85d29158940b9a761a1d287e
Favour Olukayode <nerosilva522@gmail.com>
2024-04-02 08:51:17 +0100

    chore: installed necessary librariess

---
+++ cmd/web/main.go
@@ -0,0 +1,21 @@
+package main
+
+const webPort = 8080
+
+func main() {
+
+	// connect to db
+
+	// create sessions
+
+	// create channels
+
+	// create waitgroup
+
+	//set up the application config
+
+	// setup mail
+
+	// listen for web connections
+
+}
