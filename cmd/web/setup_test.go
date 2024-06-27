947dbcf9a3fd40dc1ed422c261a1ce2e21a1af96
Favour Olukayode <nerosilva522@gmail.com>
2024-06-27 16:33:30 +0100

    chore: setup test environment

---
+++ cmd/web/setup_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"encoding/gob"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+	"github.com/the-Jinxist/subber/data"
+)
+
+var testapp AppConfig
+
+func TestMain(m *testing.M) {
+
+	gob.Register(data.User{})
+
+	session := scs.New()
+
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	testapp = AppConfig{
+		Session:       session,
+		Db:            nil,
+		InfoLog:       log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
+		ErrorLog:      log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
+		ErrorChan:     make(chan error),
+		ErrorChanDone: make(chan bool),
+		Wait:          &sync.WaitGroup{},
+	}
+
+	// create a dummy mailer
+	errorChan := make(chan error)
+	mailerChan := make(chan Message, 100)
+	doneChan := make(chan bool)
+
+	testapp.Mailer = Mail{
+		Wait:       testapp.Wait,
+		ErrorChan:  errorChan,
+		MailerChan: mailerChan,
+		DoneChan:   doneChan,
+	}
+
+	go func() {
+		select {
+		case <-testapp.Mailer.MailerChan:
+		case <-testapp.Mailer.ErrorChan:
+		case <-testapp.Mailer.DoneChan:
+			return
+		}
+
+	}()
+
+	go func() {
+		for {
+			select {
+			case err := <-testapp.ErrorChan:
+				testapp.ErrorLog.Println(err)
+			case <-testapp.ErrorChanDone:
+				return
+			}
+		}
+	}()
+
+	os.Exit(m.Run())
+}
